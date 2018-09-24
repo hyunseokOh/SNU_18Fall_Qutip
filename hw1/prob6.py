3c177c15a6f0a4bd8c3da82ab1ad58c0fdeb3556
Hyunseok <hyunseok0222@gmail.com>
2018-09-24 14:17:31 +0900

    Fix to make result file

--- hw1/prob6.py
+++ hw1/prob6.py
@@ -2,6 +2,8 @@ from qutip import *
 import numpy as np
 import matplotlib.pyplot as plt
 
+f=open("prob6_result.txt","w+")
+
 matA=np.array([[1,1],[0,1]])/np.sqrt(3)
 matB=np.array([[2,1],[-1,-2]])/np.sqrt(10)
 matC=np.array([[2,1,-1],[-2,1,0],[1,0,1]])/np.sqrt(13)
@@ -10,11 +12,13 @@ _,a,_=np.linalg.svd(matA,True,True)
 _,b,_=np.linalg.svd(matB,True,True)
 _,c,_=np.linalg.svd(matC,True,True)
 
-print("Singular values of case A : ")
-print(a)
-print()
-print("Singular values of case B : ")
-print(b)
-print()
-print("Singular values of case C : ")
-print(c)
+print("Singular values of case A : ",file=f)
+print(a,file=f)
+print(file=f)
+print("Singular values of case B : ",file=f)
+print(b,file=f)
+print(file=f)
+print("Singular values of case C : ",file=f)
+print(c,file=f)
+
+
